ab9eb8981862aedc59e03db874677c99a7785f04
bluecat101 <97800922+bluecat101@users.noreply.github.com>
2026-09-04 12:48:51 +0900

    widgetをタップしたときにアプリを起動する機能の追加

--- android/app/src/main/kotlin/com/example/girinori/RouteWidgetProvider.kt
+++ android/app/src/main/kotlin/com/example/girinori/RouteWidgetProvider.kt
@@ -2,7 +2,9 @@ package com.example.girinori
 
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.widget.RemoteViews
 import org.json.JSONArray
 
@@ -167,7 +169,20 @@ class RouteWidgetProvider : AppWidgetProvider() {
                     row
                 )
             }
-
+            // -----------------------------------------
+            // Widgetをタップしたときにアプリを起動するIntentを設定
+            // -----------------------------------------
+            val intent = Intent(context, MainActivity::class.java)
+            val pendingIntent = PendingIntent.getActivity(
+                context,
+                0,
+                intent,
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            )
+            views.setOnClickPendingIntent(
+                R.id.widget_root,
+                pendingIntent
+            )
             // -----------------------------------------
             // Widgetを更新
             // -----------------------------------------
